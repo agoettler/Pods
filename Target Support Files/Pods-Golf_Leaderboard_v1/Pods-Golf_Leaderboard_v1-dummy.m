0eb1e1511141d13c57100b75ad0e14573e212a14
AdamGary <adam-gary@uiowa.edu>
2016-10-20 15:48:20 -0500

    Initial Commit

---
+++ Target Support Files/Pods-Golf_Leaderboard_v1/Pods-Golf_Leaderboard_v1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Golf_Leaderboard_v1 : NSObject
+@end
+@implementation PodsDummy_Pods_Golf_Leaderboard_v1
+@end
---
+++ Target Support Files/Pods-Golf_Leaderboard_v1/Pods-Golf_Leaderboard_v1-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Golf_Leaderboard_v1VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Golf_Leaderboard_v1VersionString[];
+
